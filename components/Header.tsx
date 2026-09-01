ae0ac4cb0b8425a6afdfeb6e9b83cb167a035030
cypand <cypand@gmail.com>
2026-09-01 11:42:37 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -96,7 +96,7 @@ export default function Header() {
             <>
               <Link
   href="/profile"
-  className="text-white/80 text-xs
+  className="text-white/80 text-xs"
 >
   Woof, {displayName}
 </Link>
